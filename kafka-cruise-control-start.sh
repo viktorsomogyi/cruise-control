59f72d43d24ab53d6bd16f14c12f9307975d1e25
Tamas Barnabas Egyed <egyed.t@cloudera.com>
2026-09-17 00:47:53 +0200

    Bump logging versions (#2344)
    Signed-off-by: Egyed Tamas <egyed.t@cloudera.com>

--- kafka-cruise-control-start.sh
+++ kafka-cruise-control-start.sh
@@ -79,7 +79,7 @@ fi
 # Log4j settings
 if [ -z "$KAFKA_LOG4J_OPTS" ]; then
   # Log to console. This is a tool.
-  LOG4J_DIR="$base_dir/config/log4j.properties"
+  LOG4J_DIR="$base_dir/config/log4j2.properties"
   # If Cygwin is detected, LOG4J_DIR is converted to Windows format.
   (( CYGWIN )) && LOG4J_DIR=$(cygpath --path --mixed "${LOG4J_DIR}")
   KAFKA_LOG4J_OPTS="-Dlog4j.configurationFile=file:${LOG4J_DIR}"
